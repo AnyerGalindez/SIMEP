de0c3a77f6a770cd5869742a47216ff24bf44a82
Anyer Galindez <anyer.galindez22@gmail.com>
2026-08-09 22:12:12 -0400

    Commit inical del proyecto

---
+++ componenteproyecto.cpp
@@ -0,0 +1,3 @@
+#include "componenteproyecto.h"
+
+ComponenteProyecto::ComponenteProyecto() {}
---
+++ componenteproyecto.h
@@ -0,0 +1,10 @@
+#ifndef COMPONENTEPROYECTO_H
+#define COMPONENTEPROYECTO_H
+
+class ComponenteProyecto
+{
+public:
+    ComponenteProyecto();
+};
+
+#endif // COMPONENTEPROYECTO_H
