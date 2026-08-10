2d3f7fc9f7a4c3a1641936fce5728a709ee35d33
Anyer Galindez <anyer.galindez22@gmail.com>
2026-08-09 23:24:14 -0400

    Funciones de Programa listas, falta solo actividad

--- programa.cpp
+++ programa.cpp
@@ -1,3 +1,28 @@
 #include "programa.h"
 
 Programa::Programa() {}
+
+void Programa:: agregar(ComponenteProyecto* c) {
+    hijos.push_back(c);
+}
+
+int Programa:: obtenerBeneficiarios(){
+    int total=0;
+    for(int i=0 ; i<hijos.size();i++){
+
+        total+=hijos[i]->obtenerBeneficiarios();
+
+    }
+    return total;
+}
+
+void Programa::aceptar(ElementoVisitor *v){
+
+}
+
+Programa::~Programa(){
+    for(int i=0; i<hijos.size();i++){
+        delete hijos[i];
+    }
+    hijos.clear();
+}
